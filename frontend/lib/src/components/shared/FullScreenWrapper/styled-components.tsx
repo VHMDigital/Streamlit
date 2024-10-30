29c51f74bcf004e5dfd6b55f31ec9cf355f9a8e8
Nico Bellante <nico.bellante@snowflake.com>
2024-10-30 12:52:40 -0700

    removing all traces of the styled fullscreen button

--- frontend/lib/src/components/shared/FullScreenWrapper/styled-components.tsx
+++ frontend/lib/src/components/shared/FullScreenWrapper/styled-components.tsx
@@ -16,72 +16,12 @@
 
 import styled from "@emotion/styled"
 
-export interface StyledFullScreenButtonProps {
-  isExpanded: boolean
-}
-
-export const StyledFullScreenButton =
-  styled.button<StyledFullScreenButtonProps>(({ isExpanded, theme }) => {
-    const variableProps = isExpanded
-      ? {
-          right: "0.4rem",
-          top: "0.5rem",
-          backgroundColor: "transparent",
-        }
-      : {
-          right: "-3.0rem",
-          top: "-0.375rem",
-          opacity: 0,
-          transform: "scale(0)",
-          backgroundColor: theme.colors.lightenedBg05,
-        }
-
-    return {
-      position: "absolute",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-
-      zIndex: theme.zIndices.sidebar + 1,
-      //eslint-disable-next-line streamlit-custom/no-hardcoded-theme-values
-      height: "2.5rem",
-      //eslint-disable-next-line streamlit-custom/no-hardcoded-theme-values
-      width: "2.5rem",
-      transition: "opacity 300ms 150ms, transform 300ms 150ms",
-      border: "none",
-      color: theme.colors.fadedText60,
-      borderRadius: "50%",
-
-      ...variableProps,
-
-      "&:focus": {
-        outline: "none",
-      },
-
-      "&:active, &:focus-visible, &:hover": {
-        opacity: 1,
-        outline: "none",
-        transform: "scale(1)",
-        color: theme.colors.bodyText,
-        transition: "none",
-      },
-    }
-  })
-
 export interface StyledFullScreenFrameProps {
   isExpanded: boolean
 }
 
 export const StyledFullScreenFrame = styled.div<StyledFullScreenFrameProps>(
   ({ theme, isExpanded }) => ({
-    "&:hover": {
-      [StyledFullScreenButton as any]: {
-        opacity: 1,
-        transform: "scale(1)",
-        transition: "none",
-      },
-    },
-
     ...(isExpanded
       ? {
           position: "fixed",
