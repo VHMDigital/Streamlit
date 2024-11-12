786d37331a486dec47c981fae05e62f1ba4ce953
lukasmasuch <lukas.masuch@gmail.com>
2024-11-12 17:14:00 +0100

    Fix component

--- frontend/lib/src/components/elements/IFrame/IFrame.tsx
+++ frontend/lib/src/components/elements/IFrame/IFrame.tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React, { CSSProperties, ReactElement } from "react"
+import React, { ReactElement } from "react"
 
 import {
   isNullOrUndefined,
