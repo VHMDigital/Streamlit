ed30f50bee859d3cf3d9efa991e5b3aa7a98df8d
Johannes Rieke <johannes.rieke@snowflake.com>
2024-12-29 21:56:45 +0100

    Add comments

--- frontend/lib/src/components/elements/ArrowTable/ArrowTable.tsx
+++ frontend/lib/src/components/elements/ArrowTable/ArrowTable.tsx
@@ -45,6 +45,8 @@ export function ArrowTable(props: Readonly<TableProps>): ReactElement {
   return (
     <StyledTableContainer className="stTable" data-testid="stTable">
       {cssStyles && <style>{cssStyles}</style>}
+      {/* Add an extra wrapper with the border. This makes sure the border shows around
+      the entire table when scrolling horizontally. See also `styled-components.ts`. */}
       <StyledTableBorder>
         <StyledTable id={cssId} data-testid="stTableStyledTable">
           {columnHeaders.length > 0 && (
@@ -72,6 +74,13 @@ export function ArrowTable(props: Readonly<TableProps>): ReactElement {
           </tbody>
         </StyledTable>
       </StyledTableBorder>
+      {/* One negative side effect of having the border on a wrapper is that we need
+      to put the caption outside of <table>, so it shows up outside of the border. This
+      is not great for accessibility. But I think it's fine because adding captions
+      isn't a native feature (you can only do it via Pandas Styler's `set_caption`
+      function) and I couldn't find a single example on GitHub that actually does this
+      for `st.table`. We might want to revisit this if we add captions/labels as a
+      native feature or do a pass on accessibility. */}
       {caption && <caption>{caption}</caption>}
     </StyledTableContainer>
   )
