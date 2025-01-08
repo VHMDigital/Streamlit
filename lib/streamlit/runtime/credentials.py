25b239b34bb310232dd2ca6a57ae2a4e8d1f8e23
lukasmasuch <lukas.masuch@gmail.com>
2025-01-08 15:02:07 +0100

    Improve usage of built-in logging

--- lib/streamlit/runtime/credentials.py
+++ lib/streamlit/runtime/credentials.py
@@ -199,8 +199,8 @@ class Credentials:
 
         try:
             os.remove(c._conf_file)
-        except OSError as e:
-            _LOGGER.error("Error removing credentials file: %s" % e)
+        except OSError as ex:
+            _LOGGER.error("Error removing credentials file:", exc_info=ex)
 
     def save(self) -> None:
         """Save to toml file and send email."""
@@ -222,8 +222,8 @@ class Credentials:
 
         try:
             _send_email(self.activation.email)
-        except RequestException as e:
-            _LOGGER.error(f"Error saving email: {e}")
+        except RequestException as ex:
+            _LOGGER.error("Error saving email:", exc_info=ex)
 
     def activate(self, show_instructions: bool = True) -> None:
         """Activate Streamlit.
