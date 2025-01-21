f9d81e6bab450f2d800e7a24791999c99819e2f1
Maya Barnes <maya8617@gmail.com>
2025-01-21 00:08:59 -0800

    Update tested static app

--- e2e_playwright/static_app_test.py
+++ e2e_playwright/static_app_test.py
@@ -19,11 +19,13 @@ from e2e_playwright.conftest import ImageCompareFunction, wait_for_app_loaded
 
 def test_static_app(page: Page, app_port: int, assert_snapshot: ImageCompareFunction):
     """Test that a static app can be loaded"""
-    page.goto(f"http://localhost:{app_port}/?staticNotebookId=Load_CSV_from_S3")
+    page.goto(
+        f"http://localhost:{app_port}/?staticNotebookId=Hyperparameter_Tuning_with_sklearn"
+    )
     wait_for_app_loaded(page, True)
 
     app_cells = page.get_by_test_id("stExpander")
-    expect(app_cells).to_have_count(20)
+    expect(app_cells).to_have_count(24)
 
     first_cell = app_cells.nth(0)
     assert_snapshot(first_cell, name="example_static_app")
