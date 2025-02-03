b37f4b2d2a4e109ea1ab574d786a5a2a8fdf7c2f
lukasmasuch <lukas.masuch@gmail.com>
2025-02-03 15:32:22 +0100

    Add e2e test

--- e2e_playwright/st_audio_test.py
+++ e2e_playwright/st_audio_test.py
@@ -16,7 +16,7 @@ import re
 import pytest
 from playwright.sync_api import Page, expect
 
-from e2e_playwright.conftest import wait_until
+from e2e_playwright.conftest import ImageCompareFunction, wait_until
 from e2e_playwright.shared.app_utils import (
     check_top_level_class,
     click_button,
@@ -101,3 +101,13 @@ def test_audio_remount_no_autoplay(app: Page):
 def test_check_top_level_class(app: Page):
     """Check that the top level class is correctly set."""
     check_top_level_class(app, "stAudio")
+
+
+def test_audio_uses_unified_height(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Check that the audio component uses our default element height."""
+    audio_element = themed_app.get_by_test_id("stAudio").first
+
+    expect(audio_element).to_have_css("height", "40px")
+    assert_snapshot(audio_element, name="st_audio-unified_height")
