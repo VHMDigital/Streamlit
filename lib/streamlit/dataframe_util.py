e89c450ec9b79e6777445d9fee2ebdb8aaaa1e74
lukasmasuch <lukas.masuch@gmail.com>
2025-02-21 23:01:48 +0100

    Fix deprecation with fillna pandas method

--- lib/streamlit/dataframe_util.py
+++ lib/streamlit/dataframe_util.py
@@ -1260,8 +1260,11 @@ def _unify_missing_values(df: DataFrame) -> DataFrame:
     which is the only missing value type that is supported by all data
     """
     import numpy as np
+    import pandas as pd
 
-    return df.fillna(np.nan).replace([np.nan], [None]).infer_objects()
+    # Replace all recognized nulls (np.nan, pd.NA, NaT) with None
+    # then infer objects without creating a separate copy:
+    return df.replace([pd.NA, pd.NaT, np.nan], None).infer_objects(copy=False)
 
 
 def _pandas_df_to_series(df: DataFrame) -> Series[Any]:
