0fd4d7f19564b967faa8ce61aa254cf5a33a19a0
Karen Javadyan <kajarenc@gmail.com>
2025-02-25 23:41:28 +0400

    remove empty lines

--- lib/streamlit/user_info.py
+++ lib/streamlit/user_info.py
@@ -144,7 +144,6 @@ def login(provider: str | None = None) -> None:
     >>> client_id = "xxx"
     >>> client_secret = "xxx"
     >>> server_metadata_url = "https://accounts.google.com/.well-known/openid-configuration"  # fmt: skip
-    >>>
 
     Your app code:
 
@@ -216,7 +215,6 @@ def login(provider: str | None = None) -> None:
     >>> client_id = "xxx"
     >>> client_secret = "xxx"
     >>> server_metadata_url = "https://{subdomain}.okta.com/.well-known/openid-configuration"  # fmt: skip
-    >>>
 
     Your app code:
 
@@ -257,7 +255,6 @@ def login(provider: str | None = None) -> None:
     >>> client_secret = "xxx"
     >>> server_metadata_url = "https://{account}.{region}.auth0.com/.well-known/openid-configuration"  # fmt: skip
     >>> client_kwargs = { "prompt" = "login" }
-    >>>
 
     Your app code:
 
@@ -314,7 +311,6 @@ def logout() -> None:
     >>> client_id = "xxx"
     >>> client_secret = "xxx"
     >>> server_metadata_url = "https://accounts.google.com/.well-known/openid-configuration"  # fmt: skip
-    >>>
 
     Your app code:
 
