2a7d085a16ac03ac1f84fa9e064ac8cbda0304aa
lukasmasuch <lukas.masuch@gmail.com>
2025-03-13 16:36:11 +0100

    Remove one test

--- lib/tests/streamlit/runtime/runtime_test.py
+++ lib/tests/streamlit/runtime/runtime_test.py
@@ -486,32 +486,6 @@ class RuntimeTest(RuntimeTestCase):
         populate_hash_if_needed(msg)
         self.assertEqual(msg.hash, received.hash)
 
-    async def test_forwardmsg_cacheable_flag(self):
-        """Test that the metadata.cacheable flag is set properly on outgoing
-        ForwardMsgs."""
-        await self.runtime.start()
-
-        client = MockSessionClient()
-        session_id = self.runtime.connect_session(client=client, user_info=MagicMock())
-
-        with patch_config_options({"global.minCachedMessageSize": 0}):
-            cacheable_msg = create_dataframe_msg([1, 2, 3])
-            self.enqueue_forward_msg(session_id, cacheable_msg)
-            await self.tick_runtime_loop()
-
-            received = client.forward_msgs.pop()
-            self.assertTrue(cacheable_msg.metadata.cacheable)
-            self.assertTrue(received.metadata.cacheable)
-
-        with patch_config_options({"global.minCachedMessageSize": 1000}):
-            cacheable_msg = create_dataframe_msg([4, 5, 6])
-            self.enqueue_forward_msg(session_id, cacheable_msg)
-            await self.tick_runtime_loop()
-
-            received = client.forward_msgs.pop()
-            self.assertFalse(cacheable_msg.metadata.cacheable)
-            self.assertFalse(received.metadata.cacheable)
-
     async def test_duplicate_forwardmsg_caching(self):
         """Test that duplicate ForwardMsgs are sent only once."""
         with patch_config_options({"global.minCachedMessageSize": 0}):
