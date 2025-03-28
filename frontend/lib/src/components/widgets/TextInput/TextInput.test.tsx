2e455fcf14498ce9f984803a1f92f5ab9bdb36f8
Axel Carapinha <axel.carapinha@tecnico.ulisboa.pt>
2025-03-28 16:33:28 +0000

    Fixes #10115 Input widget password mgr issue (#10115)
    Describe your changes
    
    The input widget was *not* accepting input from password managers
    that needed a button click to show text in the input field.
    The issue was an `onBlur` event triggered by the autofill button,
    making the input value inconsistent with the one displayed.
    
    This fix uses the `onBlur` vs `onChange` difference and adds a flag
    to track it. Values are committed only when the flag is true,
    keeping Streamlit team optimizations from previous code.
    
    Testing Plan
    ✅ Adds unit tests to verify the fix
    
    Contribution License Agreement
    By submitting this, you agree all contributions are under
    the Apache 2.0 license.

--- frontend/lib/src/components/widgets/TextInput/TextInput.test.tsx
+++ frontend/lib/src/components/widgets/TextInput/TextInput.test.tsx
@@ -16,7 +16,13 @@
 
 import React from "react"
 
-import { act, screen, waitFor, within } from "@testing-library/react"
+import {
+  act,
+  fireEvent,
+  screen,
+  waitFor,
+  within,
+} from "@testing-library/react"
 import { userEvent } from "@testing-library/user-event"
 
 import {
@@ -358,6 +364,40 @@ describe("TextInput widget", () => {
       },
       undefined
     )
+  })
+
+  it("accepts autofilled value when focus is lost, simulating password manager autofill", async () => {
+    const user = userEvent.setup()
+    const props = getProps()
+
+    // Spy on widgetMgr
+    vi.spyOn(props.widgetMgr, "setStringValue")
+
+    render(<TextInput {...props} />)
+
+    const textInput = screen.getByRole("textbox")
+
+    await user.click(textInput)
+
+    // Simulate losing focus (onBlur) as a result of clicking in the autofill suggestion button
+    await user.tab()
+
+    // Simulate autofilling the pretended value
+    userEvent.type(textInput, "new value")
+    fireEvent.change(textInput, { target: { value: "autofilled-value" } })
+
+    // Check that the value was submitted correctly
+    await waitFor(() => {
+      expect(props.widgetMgr.setStringValue).toHaveBeenCalledWith(
+        props.element,
+        "autofilled-value",
+        { fromUi: true },
+        undefined
+      )
+    })
+
+    // Ensure the setStringValue was called twice (first time during render, second after autofill)
+    expect(props.widgetMgr.setStringValue).toHaveBeenCalledTimes(2)
   })
 
   it("resets its value when form is cleared", async () => {
