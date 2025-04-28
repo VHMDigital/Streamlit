d38326b920dc20c914e5cfa8fbcea3157d460ec7
Frida Eriksen Næss <fridaeriksenaess@gmail.com>
2025-04-28 14:41:27 +0300

    added default_tab_index and included it in layouts.py

--- lib/streamlit/elements/layouts.py
+++ lib/streamlit/elements/layouts.py
@@ -536,7 +536,10 @@ class LayoutsMixin:
 
         tab_container = self.dg._block(block_proto)
 
-        return tuple(tab_container._block(tab_proto(tab)) for tab in tabs)
+        default_index = tabs.index(default) if default else 0
+        block_proto.tab_container.default_tab_index = default_index
+
+        return tuple(tab_container._block(tab_proto(tab_label)) for tab_label in tabs)
 
     @gather_metrics("expander")
     def expander(
