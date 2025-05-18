140a5beee2104cf17a4e24e0e1668948dd631d3a
wyattscarpenter <wyattscarpenter@gmail.com>
2025-05-18 11:51:26 -0700

    actually maybe all these mocks don't apply?

--- lib/tests/streamlit/web/cli_test.py
+++ lib/tests/streamlit/web/cli_test.py
@@ -374,16 +374,7 @@ class CliTest(unittest.TestCase):
         with testutil.patch_config_options(
             {"server.prompt": prompt_mode, "server.headless": headless_mode}
         ):
-            with (
-                patch("streamlit.url_util.is_url", return_value=False),
-                patch("os.path.exists", return_value=True),
-                patch("streamlit.config.is_manually_set", return_value=False),
-                patch(
-                    "streamlit.runtime.credentials._check_credential_file_exists",
-                    return_value=False,
-                ),
-            ):
-                result = self.runner.invoke(cli, ["run", "file_name.py"], input="\n")
+            result = self.runner.invoke(cli, ["run", "file_name.py"], input="\n")
 
             self.assertNotEqual(0, result.exit_code)
             self.assertEqual(
