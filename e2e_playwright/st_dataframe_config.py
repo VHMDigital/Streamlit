d449bfafcadef94be303a10b082ed7293c22c56a
lukasmasuch <lukas.masuch@gmail.com>
2025-06-12 20:00:42 +0200

    Fix support for brackets in multi-index column names

--- e2e_playwright/st_dataframe_config.py
+++ e2e_playwright/st_dataframe_config.py
@@ -568,7 +568,7 @@ st.dataframe(
                 ("a", "b", "c"),
                 ("a", "b", "d"),
                 ("e", "f", "c"),
-                ("g", "h", "d"),
+                ("g", "h", "d (Test)"),
                 ("", "h", "i"),
                 ("j", "", ""),
             ],
@@ -796,3 +796,15 @@ st.dataframe(
         ),
     },
 )
+
+import pandas as pd
+
+import streamlit as st
+
+df_regular = pd.DataFrame({"Example": [1, 2, 3], "Example (1)": [4, 5, 6]})
+
+multi_index = pd.MultiIndex.from_tuples([("Test", "Example"), ("Test", "Example (1)")])
+df_multi = pd.DataFrame(df_regular.values, columns=multi_index)
+
+st.dataframe(df_regular)
+st.dataframe(df_multi)
