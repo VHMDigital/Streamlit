1aaf29bb7ad3d7bdcc1c9106961b2665d0b30845
lukasmasuch <lukas.masuch@gmail.com>
2025-06-14 22:08:57 +0200

    Add fix

--- e2e_playwright/st_download_button_test.py
+++ e2e_playwright/st_download_button_test.py
@@ -15,7 +15,11 @@
 import pytest
 from playwright.sync_api import Page, expect
 
-from e2e_playwright.conftest import ImageCompareFunction, wait_until
+from e2e_playwright.conftest import (
+    ImageCompareFunction,
+    wait_for_app_loaded,
+    wait_until,
+)
 from e2e_playwright.shared.app_utils import (
     check_top_level_class,
     click_checkbox,
@@ -207,6 +211,7 @@ def test_download_button_source_error(app: Page, app_port: int):
 
     # Navigate to the app
     app.goto(f"http://localhost:{app_port}")
+    wait_for_app_loaded(app)
 
     # Wait until the expected error is logged, indicating CLIENT_ERROR was sent
     wait_until(
